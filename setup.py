a0e1745cd60406e39e410f48c22a2f684908de4d
Mahmoud Abdelkader <mabdelkader@gmail.com>
2012-03-30 09:26:03 -0700

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(PATH_TO_FILE, 'README.md')) as f:
     long_description = f.read()
 
 
-VERSION = (1, 0, 1)
+VERSION = (1, 0, 2)
 
 
 # Dynamically calculate the version based on VERSION tuple
