27c05e2ec9175da19b87053fdb0306ecaf0b6650
Mahmoud Abdelkader <mabdelkader@gmail.com>
2012-03-29 18:17:01 -0700

    first commit

---
+++ citext/__init__.py
@@ -0,0 +1,58 @@
+from __future__ import unicode_literals
+
+import sqlalchemy.types as types
+
+
+class CIText(types.UserDefinedType):
+
+    def get_col_spec(self):
+        return 'CITEXT'
+
+    def bind_processor(self, dialect):
+        def process(value):
+            return value
+        return process
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            return value
+        return process
+
+
+if __name__ == '__main__':
+    from sqlalchemy import create_engine, MetaData, Integer
+    from sqlalchemy.schema import Column, Table
+    import sqlalchemy.orm as orm
+
+    engine = create_engine('postgresql://balanced:balanced@localhost/balanced')
+    meta = MetaData()
+
+    test_table = Table('test', meta,
+        Column('id', Integer(), primary_key=True),
+        Column('txt', CIText()))
+
+    conn = engine.connect()
+
+    meta.bind = conn
+    meta.drop_all()
+    meta.create_all()
+
+    class TestObj(object):
+        def __init__(self, id_, txt):
+            self.id = id_
+            self.txt = txt
+
+        def __repr__(self):
+            return "TestObj(%r, %r)" % (self.id, self.txt)
+
+    orm.mapper(TestObj, test_table)
+    Session = orm.sessionmaker(bind=engine)
+    ses = Session()
+
+    to = TestObj(1, txt='FooFighter')
+    ses.add(to)
+    ses.commit()
+    row = ses.query(TestObj).filter(TestObj.txt == 'foofighter').all()
+    assert len(row) == 1
+    print row
+    ses.close()
---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+PATH_TO_FILE = os.path.dirname(__file__)
+
+
+with open(os.path.join(PATH_TO_FILE, 'README.rst')) as f:
+    long_description = f.read()
+
+
+VERSION = (1, 0, 0)
+
+
+# Dynamically calculate the version based on VERSION tuple
+if len(VERSION) > 2 and VERSION[2] is not None:
+    str_version = "%s.%s_%s" % VERSION[:3]
+else:
+    str_version = "%s.%s" % VERSION[:2]
+
+
+version = str_version
+
+
+setup(
+    name='sqlalchemy-citext',
+    version=version,
+    description="A sqlalchemy plugin that allows postgres use of CITEXT.",
+    long_description=long_description,
+    author='Mahmoud Abdelkader',
+    author_email='mabdelkader@gmail.com',
+    url='https://github.com/mahmoudimus/sqlalchemy-citext',
+    install_requires=[
+        'SQLAlchemy>=0.6',
+        'psycopg2==2.4.5',
+    ],
+    setup_requires=[],
+    zip_safe=False,
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
